a859138ef9b29455ab1c6ad353bf7b2af48ca184
123-abc-debug1 <2153471051@qq.com>
2026-04-28 01:36:24 +0800

    Step4: Add player name input and winner

--- reaction.py
+++ reaction.py
@@ -1,23 +1,32 @@
 from machine import Pin
 from time import sleep
 from random import uniform
-import sys  # 新增这行
+import sys
 
 # Initialize components
 led = Pin(4, Pin.OUT)
-left_button = Pin(14, Pin.IN, Pin.PULL_UP)  # 新增这行
-right_button = Pin(15, Pin.IN, Pin.PULL_UP)  # 新增这行
+left_button = Pin(14, Pin.IN, Pin.PULL_UP)
+right_button = Pin(15, Pin.IN, Pin.PULL_UP)
+
+# Get player names
+left_name = input('left player name is ')  # 新增这行
+right_name = input('right player name is ')  # 新增这行
+
+print("\nGame about to start...")
+print("LED is on, waiting for it to turn off...")
 
 # Core game logic
 led.value(1)
 sleep(uniform(5, 10))
 led.value(0)
 
-# Wait for button press
+print("\nLED is off! Press the button quickly!")
+
+# Wait for button press and determine winner
 while True:
     if left_button.value() == 0:
-        print("Left button pressed!")
+        print(f"\n🎉 {left_name} won the game!")  # 修改这行
         sys.exit()
     if right_button.value() == 0:
-        print("Right button pressed!")
+        print(f"\n🎉 {right_name} won the game!")  # 修改这行
         sys.exit()
